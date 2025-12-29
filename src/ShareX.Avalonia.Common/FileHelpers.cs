4eec0bab075e6d9eefdf820fe758e4ac942870fd
Michael D <McoreD@users.noreply.github.com>
2025-12-29 20:05:50 +0800

    Add file extension helpers

--- src/ShareX.Avalonia.Common/FileHelpers.cs
+++ src/ShareX.Avalonia.Common/FileHelpers.cs
@@ -24,13 +24,21 @@
 #endregion License Information (GPL v3)
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace ShareX.Avalonia.Common
 {
     public static class FileHelpers
     {
+        public static readonly string[] ImageFileExtensions = { "jpg", "jpeg", "png", "gif", "bmp", "ico", "tif", "tiff" };
+        public static readonly string[] TextFileExtensions = { "txt", "log", "nfo", "c", "cpp", "cc", "cxx", "h", "hpp", "hxx", "cs", "vb",
+            "html", "htm", "xhtml", "xht", "xml", "css", "js", "php", "bat", "java", "lua", "py", "pl", "cfg", "ini", "dart", "go", "gohtml" };
+        public static readonly string[] VideoFileExtensions = { "mp4", "webm", "mkv", "avi", "vob", "ogv", "ogg", "mov", "qt", "wmv", "m4p",
+            "m4v", "mpg", "mp2", "mpeg", "mpe", "mpv", "m2v", "flv", "f4v" };
+
         public static string ExpandFolderVariables(string path, bool supportCustomSpecialFolders = false)
         {
             if (string.IsNullOrEmpty(path))
@@ -103,6 +111,73 @@ namespace ShareX.Avalonia.Common
             }
 
             return path;
+        }
+
+        public static string GetFileNameExtension(string filePath, bool includeDot = false, bool checkSecondExtension = true)
+        {
+            string extension = string.Empty;
+
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                int pos = filePath.LastIndexOf('.');
+
+                if (pos >= 0)
+                {
+                    extension = filePath.Substring(pos + 1);
+
+                    if (checkSecondExtension)
+                    {
+                        filePath = filePath.Remove(pos);
+                        string extension2 = GetFileNameExtension(filePath, false, false);
+
+                        if (!string.IsNullOrEmpty(extension2))
+                        {
+                            foreach (string knownExtension in new[] { "tar" })
+                            {
+                                if (extension2.Equals(knownExtension, StringComparison.OrdinalIgnoreCase))
+                                {
+                                    extension = extension2 + "." + extension;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (includeDot)
+                    {
+                        extension = "." + extension;
+                    }
+                }
+            }
+
+            return extension;
+        }
+
+        public static bool CheckExtension(string filePath, IEnumerable<string> extensions)
+        {
+            string ext = GetFileNameExtension(filePath);
+
+            if (!string.IsNullOrEmpty(ext))
+            {
+                return extensions.Any(x => ext.Equals(x, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return false;
+        }
+
+        public static bool IsImageFile(string filePath)
+        {
+            return CheckExtension(filePath, ImageFileExtensions);
+        }
+
+        public static bool IsTextFile(string filePath)
+        {
+            return CheckExtension(filePath, TextFileExtensions);
+        }
+
+        public static bool IsVideoFile(string filePath)
+        {
+            return CheckExtension(filePath, VideoFileExtensions);
         }
 
         public static void CreateDirectory(string directoryPath)
