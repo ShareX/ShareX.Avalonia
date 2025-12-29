9523265a2e27a24f1c43851a6711a060bf3d722e
Michael D <McoreD@users.noreply.github.com>
2025-12-29 13:01:47 +0800

    Add Avalonia ImageEffects adjustments

---
+++ src/ShareX.Avalonia.ImageEffects/Adjustments/Inverse.cs
@@ -0,0 +1,44 @@
+#region License Information (GPL v3)
+
+/*
+    ShareX.Avalonia - The Avalonia UI implementation of ShareX
+    Copyright (c) 2007-2025 ShareX Team
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+
+#endregion License Information (GPL v3)
+
+using ShareX.Avalonia.Common;
+using ShareX.Avalonia.Common.Extensions;
+using ShareX.Avalonia.ImageEffects.Helpers;
+using System.Drawing;
+
+namespace ShareX.Avalonia.ImageEffects
+{
+    internal class Inverse : ImageEffect
+    {
+        public override Bitmap Apply(Bitmap bmp)
+        {
+            using (bmp)
+            {
+                return ColorMatrixManager.Inverse().Apply(bmp);
+            }
+        }
+    }
+}
+
