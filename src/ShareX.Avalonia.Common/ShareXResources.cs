f80ad00caa265eca82222a2944329a336f762c97
Michael D <McoreD@users.noreply.github.com>
2026-01-10 21:55:32 +0800

    Use dynamic assembly version in ShareXResources
    Replaced the hardcoded version string in ShareXResources with a dynamically retrieved version from the entry assembly. Updated Program.cs to use the new Version property from ShareXResources for consistent version reporting.

--- src/ShareX.Avalonia.Common/ShareXResources.cs
+++ src/ShareX.Avalonia.Common/ShareXResources.cs
@@ -29,8 +29,15 @@ namespace XerahS.Common
     {
         public const string AppName = "XerahS";
         public const string ProductName = AppName;
-        public const string Version = "v0.2.0";
-        public const string ProductNameWithVersion = ProductName + " " + Version;
+        
+        public static readonly string Version = GetVersion();
+        public static readonly string ProductNameWithVersion = ProductName + " " + Version;
+
+        private static string GetVersion()
+        {
+            var version = System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version;
+            return version != null ? $"v{version.Major}.{version.Minor}.{version.Build}" : "v0.0.0";
+        }
         public const string HistoryFolderName = "History";
         public const string HistoryFileName = "History.db";
         public static string UserAgent => AppName;
